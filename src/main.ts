902c930e7225fe1a42cf0c7e4cc3ed8461c18824
Ikoriantsoa <rsfmamboara@gmail.com>
2025-03-10 19:13:39 +0300

    Initialisation du microservice entry_point

---
+++ src/main.ts
@@ -0,0 +1,35 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { ValidationPipe } from '@nestjs/common';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.TCP,
+    options: {
+      host: process.env.HOST,
+      port: Number(process.env.PORT_APPRENANT),
+    }
+  });
+
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.TCP,
+    options: {
+      host: process.env.HOST,
+      port: Number(process.env.PORT_ENTREPRISE),
+    }
+  });
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  await app.listen(Number(process.env.PORT));
+}
+bootstrap();
